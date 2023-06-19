57063a879f7fbd8dc915fde05ed9fbbc179cc109
max <rew221rew@gmail.com>
2023-06-19 14:33:46 +0300

    lab2

---
+++ Lab2/child.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    int* newArray = new int[argc];
+    for (int i = 0, l = 0, r = 1; i < argc; i++)
+    {
+        int number = atoi(argv[i]);
+        for (int j=2; j <= sqrt((double)number); j++)
+        {
+            if (number%j == 0 && number != j)
+            {
+                newArray[argc - r] = number;
+                ++r;
+                number = 0;
+
+                break;
+            }
+        }
+        if (number != 0)
+        {
+            newArray[l] = number;
+            ++l;
+        }
+    }
+    for (int i = 0; i < argc; i++)
+    {
+        cout << newArray[i] << " ";
+    }
+    cout << "\n";
+    system("pause");
+    return 0;
+}
---
+++ Lab2/parent.cpp
@@ -0,0 +1,30 @@
+﻿#include <windows.h>
+#include <conio.h>
+#include <cstdlib>
+#include <time.h>
+#include <string>
+using namespace std;
+int main()
+{
+    wchar_t lpszAppName[] = L"D:\\Microsoft VS Code Projects\\C++\\OS\\Lab2\\child.exe";
+    STARTUPINFOW si;
+    PROCESS_INFORMATION piCom;
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+    si.lpTitle = L"console";
+	srand(unsigned(time(0)));
+    int size = rand() % 20 + 1;
+    wstring temp = to_wstring(rand() % 100);
+    for (int i = 1; i < size; i++)
+    {
+        temp += L" " + to_wstring(rand() % 100);
+    }
+    wchar_t* lpszCommandLine = new wchar_t[temp.length()];
+    wcscpy_s(lpszCommandLine, temp.length() + 1, temp.c_str());
+	CreateProcessW(lpszAppName, lpszCommandLine, NULL, NULL, FALSE,
+			CREATE_NEW_CONSOLE, NULL, NULL, &si, &piCom);
+    WaitForSingleObject(piCom.hProcess, INFINITE);
+	CloseHandle(piCom.hThread);
+	CloseHandle(piCom.hProcess);
+    return 0;
+}
