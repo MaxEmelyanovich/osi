39f2ef4d7fffbdc6bf4ce3ec6082fa3f5e20c17e
max <rew221rew@gmail.com>
2023-06-19 14:36:12 +0300

    lab5

---
+++ Lab5/server.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <windows.h>
+#pragma comment(lib, "kernel32.lib")
+
+using namespace std;
+
+int main() {
+
+    STARTUPINFOW *si;
+    PROCESS_INFORMATION *pi;
+    HANDLE hWritePipe, hReadPipe, hServerEvent, hSimpleEvent, hSimpleEndedEvent;
+    SECURITY_ATTRIBUTES sa;
+
+    hServerEvent = CreateEventW(nullptr, FALSE, FALSE, L"ServerEvent");
+    if (!hServerEvent) {
+        cout << "ServerEvent was not created\n";
+        return 1;
+    }
+
+    hSimpleEvent = CreateEventW(nullptr, FALSE, FALSE, L"SimpleEvent");
+    if (!hSimpleEvent) {
+        cout << "SimpleEvent was not created\n";
+        return 1;
+    }
+
+    hSimpleEndedEvent = CreateEventW(nullptr, FALSE, FALSE, L"SimpleEndedEvent");
+    if (!hSimpleEndedEvent) {
+        cout << "SimpleEndedEvent was not created\n";
+        return 1;
+    }
+
+    HANDLE hEvents[] = {hServerEvent, hSimpleEndedEvent};
+
+    sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+    sa.lpSecurityDescriptor = nullptr;
+    sa.bInheritHandle = TRUE;
+
+    if (!CreatePipe(&hReadPipe, &hWritePipe, &sa, 0)) {
+        cout << "Pipe was not created\n";
+        return 1;
+    }
+
+    int size;
+    cout << "Enter array size: ";
+    cin >> size;
+
+    si = new STARTUPINFOW;
+    ZeroMemory(si, sizeof(STARTUPINFOW));
+    si->cb = sizeof(STARTUPINFOW);
+    pi = new PROCESS_INFORMATION;
+    wstring temp;
+    temp = to_wstring(reinterpret_cast<intptr_t>(hWritePipe)) + L" " + 
+        to_wstring(reinterpret_cast<intptr_t>(hReadPipe));
+    wchar_t* commLine = new wchar_t[temp.length()];
+    wcscpy_s(commLine, temp.length() + 1, temp.c_str());
+
+    if (!CreateProcessW(L"simple.exe", commLine, nullptr, nullptr, TRUE, 
+    CREATE_NEW_CONSOLE, nullptr, nullptr, si, pi)) {
+        cout << "Process creation failed\n";
+        cout << GetLastError() << "\n";
+        return 1;
+    }
+
+    DWORD dwBytesWritten;
+    if (!WriteFile(hWritePipe, &size, sizeof(int), &dwBytesWritten, nullptr)) {
+        cout << "Info was not written\n";
+        return 1;
+    }
+
+    SetEvent(hSimpleEvent);
+
+    DWORD dwBytesRead = 0;
+    __int32 res;
+    while (true)
+    {
+        int eventIndex = WaitForMultipleObjects(2, hEvents, FALSE, INFINITE) - WAIT_OBJECT_0;
+        if (eventIndex == 1)
+        {
+            break;
+        }
+
+        if (!ReadFile(hReadPipe, &res, sizeof(__int32), &dwBytesRead, nullptr)) {
+            cout << "Info was not read\n";
+            return 1;
+        }
+
+        cout << res << " " << "\n";
+        SetEvent(hSimpleEvent);
+    }
+
+    CloseHandle(hWritePipe);
+    CloseHandle(hReadPipe);
+    CloseHandle(hServerEvent);
+    CloseHandle(hSimpleEvent);
+    CloseHandle(hSimpleEndedEvent);
+    WaitForSingleObject(pi->hProcess, INFINITE);
+    CloseHandle(pi->hProcess);
+    CloseHandle(pi->hThread);
+    delete[] commLine;
+    return 0;
+}
---
+++ Lab5/simple.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include <cmath>
+#pragma comment(lib, "kernel32.lib")
+
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+
+    int sqrtn = sqrt(n);
+
+    bool prime[n + 1];
+    for (int i = 2; i <= n; i++) {
+        prime[i] = true;
+    }
+
+    for (int i = 2; i <= sqrtn; i++) {
+        if (prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    return prime[n];
+}
+
+int main(int argc, char* argv[]) {
+	srand(time(0));
+	HANDLE hWritePipe = (HANDLE)(atoi(argv[0]));
+	HANDLE hReadPipe = (HANDLE)(atoi(argv[1]));
+
+
+	if (hReadPipe == INVALID_HANDLE_VALUE || hWritePipe == INVALID_HANDLE_VALUE) {
+		printf("Error: Invalid handle value\n");
+		return 1;
+	}
+
+	HANDLE hServerEvent = OpenEventW(EVENT_MODIFY_STATE, FALSE, L"ServerEvent");
+	if (!hServerEvent) {
+		cout << "ServerEvent was not read\n";
+        return 1;
+	}
+
+    HANDLE hSimpleEvent = OpenEventW(EVENT_MODIFY_STATE, FALSE, L"SimpleEvent");
+	if (!hSimpleEvent) {
+		cout << "ServerEvent was not read\n";
+        return 1;
+	}
+
+    HANDLE hSimpleEndedEvent = OpenEventW(EVENT_MODIFY_STATE, FALSE, L"SimpleEndedEvent");
+    if (!hSimpleEndedEvent) {
+        cout << "SimpleEndedEvent was not created\n";
+        return 1;
+    }
+
+    WaitForSingleObject(hSimpleEvent, INFINITE);
+
+	int size;
+	DWORD dwBytesRead;
+	if (!ReadFile(hReadPipe, &size, sizeof(int), &dwBytesRead, NULL)) {
+		cout << "Info was not read";
+		cout << GetLastError();
+        return 1;
+	}
+
+    cout << "Array: ";
+	__int32* arr = new __int32[size];
+	for (int i = 0; i < size; ++i) {
+	    cout << (arr[i] = rand() % 100 + 1) << " ";
+	}
+	cout << '\n';
+
+    cout << "Prime numbers:\n";
+    DWORD dwBytesWritten;
+    ungetc('a', stdin);
+    for (int i = 0; i < size; ++i)
+    {
+        if (isPrime(arr[i]))
+        {
+            getchar();
+            if (!WriteFile(hWritePipe, &arr[i], sizeof(__int32), &dwBytesWritten, NULL)) {
+                cout << "Number was not written";
+            }
+            cout << arr[i];
+            SetEvent(hServerEvent);
+            WaitForSingleObject(hSimpleEvent, INFINITE);
+        }
+    }
+
+    cout << '\n';
+
+	SetEvent(hSimpleEndedEvent);
+    system("pause");
+	CloseHandle(hWritePipe);
+	CloseHandle(hReadPipe);
+	CloseHandle(hServerEvent);
+    CloseHandle(hSimpleEvent);
+    CloseHandle(hSimpleEndedEvent);
+    delete[] arr;
+	return 0;
+}
